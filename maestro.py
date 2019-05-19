7bef211301e9a1ecf2073535a3a0c36286bd478e
Charles Blackburn <stormchaseruk@gmail.com>
2019-05-19 16:50:08 -0400

    added Maestro Controller Module and basic unit tests

---
+++ maestro.py
@@ -0,0 +1,104 @@
+# Pololu Maestro Servo class
+# baed on the work by martin saint: http://martinsant.net/?page_id=479
+#
+import serial
+import time
+
+class MaestroController:
+
+    #Class constructor
+    def __init__(self, usbPort):
+        self.usbPort = usbPort
+        # Set the lower, upper and range limits for each servo channel
+        # min ms, center ms, max ms, servo throw
+        self.ServoLimits = [ [600, 1600, 2400, 270],
+                            [1000, 1500, 2000, 180],
+                            [1000, 1500, 2000, 180],
+                            [1000, 1500, 2000, 180],
+                            [1000, 1500, 2000, 180],
+                            [1000, 1500, 2000, 180]  ]
+
+        # Port that the maestro is connected to = usually TTYACM0/1
+        self.sc = serial.Serial(self.usbPort, timeout=1)
+
+    # Class Destructor
+    def closeServo(self):
+        self.sc.close()
+
+    def CenterServos(self):
+        self.setAngle(0, 0)
+        self.setAngle(1, 0)
+        self.setAngle(2, 0)
+        self.setAngle(3, 0)
+        self.setAngle(4, 0)
+        self.setAngle(5, 0)
+
+    # Set an angle between -90 (full min to +90 full max, with 0 degrees as center position
+    # This checks the servo limits above to make sure it doesn't go past those
+    # converts the range that the servo can go so that it is always -90 to +90 degrees
+    # with 0 degrees being "center". 
+    # for example, a -90 on a 270 degree servo is always going to be the max the servo can go.
+    def setAngle(self, chan, angle):
+        # Check limits
+        if angle == 0:
+            ms = self.ServoLimits[chan][1]
+        elif angle == -90:
+            ms = self.ServoLimits[chan][0]
+        elif angle == 90:
+            ms = self.ServoLimits[chan][2]
+        else:
+            # Get new range for angle
+            old_min = -90
+            old_max = 90
+            new_max = self.ServoLimits[chan][2]
+            new_min = self.ServoLimits[chan][0]
+    
+            OldRange = (old_max - old_min)
+            NewRange = (new_max - new_min)
+    
+            ms = (((angle - old_min) * NewRange) / OldRange) + new_min
+
+        # Set the position according to the calculated ms
+        print("Set servo channel {} to {} degrees ({} ms)".format(chan, angle, ms))
+        self.setPosition(chan, ms)
+
+
+    # set definitive angle via maestro
+    # Alternative to above if you are using standard 180 degree servos
+    # this is the direct controlled version via pololu commands
+    def setDefAngle(self, n, angle):
+        if angle > 180 or angle <0:
+           angle=90
+        bud=chr(0xFF)+chr(n)+chr(byteone)
+        self.sc.write(bud)
+
+    # Set the servo channel to a specified ms value
+    def setPosition(self, servo, microsecs):
+        microsecs = microsecs * 4
+        poslo = (microsecs & 0x7f)
+        poshi = (microsecs >> 7) & 0x7f
+        chan  = servo &0x7f
+        data =  chr(0xaa) + chr(0x0c) + chr(0x04) + chr(chan) + chr(poslo) + chr(poshi)
+        self.sc.write(data)
+
+    # get the current position of the servo as a hi/lo nibble pair
+    def getPosition(self, servo):
+        chan  = servo &0x7f
+        data =  chr(0xaa) + chr(0x0c) + chr(0x10) + chr(chan)
+        self.sc.write(data)
+        w1 = ord(self.sc.read())
+        w2 = ord(self.sc.read())
+        return w1, w2
+
+    # Get error codes as a hi/lo nibble pair
+    def getErrors(self):
+        data =  chr(0xaa) + chr(0x0c) + chr(0x21)
+        self.sc.write(data)
+        w1 = ord(self.sc.read())
+        w2 = ord(self.sc.read())
+        return w1, w2
+
+    # Trigger a specified subrouting stored in the maestro
+    def triggerScript(self, subNumber):
+        data =  chr(0xaa) + chr(0x0c) + chr(0x27) + chr(0)
+        self.sc.write(data)
--- mwp_at.py
+++ mwp_at.py
@@ -1,4 +1,3 @@
-#:ts=4 number
 #!/usr/bin/python
 
 # Simple python example for mwp dbus
@@ -6,12 +5,16 @@
 import sys
 import dbus
 import dbus.mainloop.glib
-
+import time
+import atexit
 from gi.repository import GLib
+
+from maestro import MaestroController
+from haversine import haversine
 
 # GLobal data for posits etc
 posit_HOME = ""
-posit_UAV  = ""
+posit_UAV = ""
 posit_SATS = ""
 posit_STAT = ""
 
@@ -26,98 +29,134 @@ def home_handler(*args):
 
 # Handler for number of sats chcanged
 def SatsChanged_handler(*args):
-  print ("Sattellites Changed: ", args)
+    print ("Sattellites Changed: ", args)
 
 # Handler for UAV state change - eg arming
 def StateChanged_handler(*args):
-	print("State Changed: ", args)
+    print("State Changed: ", args)
 
 def MWP_GetDevices(mwp_obj):
-	return mwp_obj.GetDevices()
+    return mwp_obj.GetDevices()
 
 # Get Current Connection Status
 def MWP_GetConnStatus(*args):
-	return mwp_obj.ConnectionStatus()
+    return mwp_obj.ConnectionStatus()
 
 def MWP_Connect(mwp_obj):
-	# Get Number of devices
-	device_list = MWP_GetDevices(mwp_obj)
+    # Get Number of devices
+    device_list = MWP_GetDevices(mwp_obj)
 
-	# Are we Connected?
-	constat = MWP_GetConnStatus(mwp_obj)
+    # Are we Connected?
+    constat = MWP_GetConnStatus(mwp_obj)
 
-	# If we are just return true, if not, try to connect and if still not connected, return false
-	if constat[1]:
-		return(True)
-	else:
-		# Are we connected yet?
-		constat = mwp.ConnectDevice(device_list[0])
-		if constat[1]:
-			return(True)
-		else:
-			return(False)
+    # If we are just return true, if not, try to connect and if still not connected, return false
+    if constat[1]:
+        return (True)
+    else:
+        # Are we connected yet?
+        constat = mwp.ConnectDevice(device_list[0])
+        if constat[1]:
+            return (True)
+        else:
+            return (False)
 
 # Upload a mission to the FC
 def UploadMission(mwp_obj, filename):
-	# Are we Connected? If not, try and connect else fail
-	constat = MWP_Connect(mwp_obj)
-	if constat[1]:
-		# Try to open mission
-		mwp_mission_points = mwp_obj.Loadmission(filename)
-		print("Loaded {} wth {} Mission Points".format(filename, str(mwp_mission_points)))
-		# Upload Mission
-		if mwp_mission_points > 0:
-			nwpts = mwp_obj.UploadMission(True)
-			print("Uploaded {} Waypoints to UAV".format(wpts))
+    # Are we Connected? If not, try and connect else fail
+    constat = MWP_Connect(mwp_obj)
+    if constat[1]:
+        # Try to open mission
+        mwp_mission_points = mwp_obj.Loadmission(filename)
+        print("Loaded {} wth {} Mission Points".format(filename, str(mwp_mission_points)))
+        # Upload Mission
+        if mwp_mission_points > 0:
+            nwpts = mwp_obj.UploadMission(True)
+            print("Uploaded {} Waypoints to UAV".format(wpts))
 
 # Initalise mwp and get devices
 def MWP_Setup(mwp_obj):
-	print("Getting MWP Device List")
-	# Get List of Devices
-	device_list = MWP_GetDevices(mwp)
-	for dev in device_list:
-		print("Found Device: {}".format(dev))
+    print("Getting MWP Device List")
+    # Get List of Devices
+    device_list = MWP_GetDevices(mwp)
+    for dev in device_list:
+        print("Found Device: {}".format(dev))
 
-	return(device_list)
+    return (device_list)
 
 # Initialise the tracker post data
 def MWP_InitTracker(mwp_obj):
-	global posit_HOME, posit_UAV
+    global posit_HOME, posit_UAV
 
-	posit_HOME = mwp_obj.GetHome()
-	posit_UAV  = mwp_obj.GetLocation()
-	posit_SATS = mwp_obj.GetSats()
-	print("Init Home: {}".format(posit_HOME))
-	print("Init Vehicle: {}".format(posit_UAV))
-	print("Init Sats: {}".format(posit_SATS))
+    posit_HOME = mwp_obj.GetHome()
+    posit_UAV = mwp_obj.GetLocation()
+    posit_SATS = mwp_obj.GetSats()
+    print("Init Home: {}".format(posit_HOME))
+    print("Init Vehicle: {}".format(posit_UAV))
+    print("Init Sats: {}".format(posit_SATS))
 
 # Connect to the signals we need for location data
 def MWP_SignalSubscribe(mwp_obj):
-	mwp_obj.connect_to_signal('LocationChanged', loc_handler)
-	mwp_obj.connect_to_signal('HomeChanged', home_handler)
-	mwp_obj.connect_to_signal('SatsChanged', SatsChanged_handler)
-	mwp_obj.connect_to_signal('StateChanged', StateChanged_handler)
+    mwp_obj.connect_to_signal('LocationChanged', loc_handler)
+    mwp_obj.connect_to_signal('HomeChanged', home_handler)
+    mwp_obj.connect_to_signal('SatsChanged', SatsChanged_handler)
+    mwp_obj.connect_to_signal('StateChanged', StateChanged_handler)
 
-#####################################################################3
-### MAIN
-#####################################################################3
+def MaestroCycleChannel(maestro, channel):
+    print("Cycling servo on channel {}".format(channel))
+    maestro.setAngle(channel, -90)
+    time.sleep(0.25)
+    maestro.setAngle(channel, 90)
+    time.sleep(0.25)
+    maestro.setAngle(channel, 0)
+
+# MAIN
 if __name__ == "__main__":
-	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+    # Object for maestro controller
+    Maestro_obj = None
 
-	# Attempting DBUS Connection to MWP
-	print("Attempting DBUS Connection to MWP")
-	try:
-	    bus = dbus.SessionBus()
-	    obj = bus.get_object("org.mwptools.mwp", "/org/mwptools/mwp")
-	    mwp = dbus.Interface(obj, "org.mwptools.mwp")
+    # Check for serial ports on command line
+    # format: mwp_at.py <maestro tty port>
+    # Maestros expose 2 TTYs. usually ttyACM0 for the standard port
+    # and ttyACM1 for the TTL port We are only using the standard port (ttyACO0)
+    try:
+        if len(sys.argv) < 2:
+            print("Wrong amount of arguments: {}".format(len(sys.argv)))
+            print("USAGE:")
+            print("{} <tty port for maestro controller - usually /dev/ttyACM0".format(sys.argv[0]))
+            exit(1)
+        else:
+            MaestroTTY = sys.argv[1]
+            print("Using {} for Maestro controller".format(MaestroTTY))
+    except:
+        pass
 
-	except dbus.DBusException as e:
-	        print(str(e))
-	        sys.exit(255)
+    # Create the maestro object and center channel
+    try:
+        Maestro_obj = MaestroController(MaestroTTY)
+        print("Centering all channels on {}".format(MaestroTTY))
+        MaestroCycleChannel(Maestro_obj, 0)
+        MaestroCycleChannel(Maestro_obj, 1)
 
+    finally:
+        atexit( Maestro_obj.closeServo())
+        
+    print("Creating DBUS Loop")
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
-	# Get List of MWP Devices
-	MWP_Devices = MWP_Setup(mwp);	
+    # Attempting DBUS Connection to MWP
+    print("Attempting DBUS Connection to MWP")
+    try:
+        bus = dbus.SessionBus()
+        obj = bus.get_object("org.mwptools.mwp", "/org/mwptools/mwp")
+        mwp = dbus.Interface(obj, "org.mwptools.mwp")
 
-	loop = GLib.MainLoop()
-	loop.run()
+    except dbus.DBusException as e:
+        print(str(e))
+        Maestro_obj.closeServo()
+        sys.exit(255)
+
+    # Get List of MWP Devices
+    MWP_Devices = MWP_Setup(mwp)
+
+    loop = GLib.MainLoop()
+    loop.run()
